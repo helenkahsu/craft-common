be50a39173d3a2bfae8213a373ff9cb384cfebc5
helen <you@example.com>
2021-10-21 11:16:59 -0500

    Areamethod

--- areaproject/src/areaproject/CalculateArea.java
+++ areaproject/src/areaproject/CalculateArea.java
@@ -3,8 +3,12 @@ package areaproject;
 public class CalculateArea {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+Area(4,5);
+	} 
+	public static void Area(int x,int y) {
+		int area= x*y;
+		System.out.println(area);
 	}
 
 }
